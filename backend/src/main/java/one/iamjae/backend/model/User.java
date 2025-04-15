b4625e095a965c6afe46eca5ff2eeb5ffafc9bc0
Jaewon Chung <jaewonchung97@gmail.com>
2025-04-16 07:20:12 +0900

    Updated

--- backend/src/main/java/one/iamjae/backend/model/User.java
+++ backend/src/main/java/one/iamjae/backend/model/User.java
@@ -10,8 +10,6 @@ public class User {
     private String name;
     private String email;
 
-    public User() {}
-
     public User(Long id, String name, String email) {
         this.id = id;
         this.name = name;
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,25 @@
+import {useEffect, useState} from "react";
+
 function App() {
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        fetch('http://192.168.0.11:8080/api/users') // 포트 확인! docker-compose에 따라 바뀔 수 있음
+            .then((res) => res.json())
+            .then((data) => setUsers(data))
+            .catch((err) => console.error('Error fetching users:', err));
+    }, []);
+
+    useEffect(() => {
+        console.log(users);
+    }, [users]);
 
   return (
       <div className={`text-3xl bg-yellow`}>
-          <span className={`text-sm`}>Bye World</span>
+          <ul>
+              {users.map((user) => (
+                  <li key={user.id}>{user.name} ({user.email})</li>
+              ))}
+          </ul>
       </div>
   )
 }
